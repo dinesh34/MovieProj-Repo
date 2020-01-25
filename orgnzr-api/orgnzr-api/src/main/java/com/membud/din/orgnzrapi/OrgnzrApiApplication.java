0ec4f159e8bf4dfc659b899536782702f9d9f161
Dinesh de Silva <dinesh.de.silva@ifsworld.com>
2020-01-25 19:39:16 +0530

    First Commit for Project

---
+++ orgnzr-api/orgnzr-api/src/main/java/com/membud/din/orgnzrapi/OrgnzrApiApplication.java
@@ -0,0 +1,13 @@
+package com.membud.din.orgnzrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgnzrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgnzrApiApplication.class, args);
+	}
+
+}
